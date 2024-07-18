2e081edb2ce051cc2f965a378e1fe26edda5bc97
Luciano Montilla <unonueve95@gmail.com>
2024-07-18 10:37:26 -0300

    Small UI fixes

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -16,11 +16,12 @@ export function Button({ variant, ...props }: ButtonProps) {
 }
 
 const buttonVariants = cva(
-    "inline-flex items-center justify-center gap-1 px-4 py-2 text-sm rounded-full drop-shadow-lg transition duration-150 font-medium group",
+    "inline-flex items-center w-full md:w-max justify-center gap-1 px-4 py-2 text-sm rounded-full hover:drop-shadow-lg transition-all duration-150 font-medium group",
     {
         variants: {
             variant: {
-                primary: "bg-red-500 text-white hover:ring hover:ring-red-300",
+                primary:
+                    "bg-red-500 text-white hover:bg-transparent hover:text-red-500 hover:ring-2 hover:ring-red-500",
                 secondary:
                     "transparent border-2 border-red-500 text-red-500 hover:bg-red-600 hover:text-white hover:border-red-500",
             },
